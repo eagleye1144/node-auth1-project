1f428478c3297b1619963a5d1929c5ac95e1e31b
Mitchell Walker <eagleye1144@gmail.com>
2021-11-08 16:20:41 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -21,7 +21,7 @@ async function checkUsernameFree(req, res, next) {
       next()
     }
     else {
-      next({"message": "Username taken"})
+      next({"message": "Username taken", status: 422})
     }
   } catch (error) {
     next(error)
@@ -36,8 +36,18 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next()
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({username: req.body.username})
+    if (!users.length) {
+      next()
+    }
+    else {
+      next({"message": "Invalid credentials", status: 401})
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 /*
@@ -49,7 +59,11 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422})
+  } else {
+    next()
+  }
 }
 
 module.exports = {
